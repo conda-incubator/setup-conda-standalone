40492b32cedbca8f55d8af3f78326471df928bff
Marco Esters <mesters@anaconda.com>
2026-03-04 14:11:26 -0800

    Add missing top-level files

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+/**
+ * A map that converts NodeJS architecture designations
+ * to conda channel subdirectories (platforms)
+ */
+export const condaPlatforms: Record<string, string> = {
+  'darwin-arm64': 'osx-arm64',
+  'darwin-x64': 'osx-64',
+  'linux-arm64': 'linux-aarch64',
+  'linux-ppc64': 'linux-ppc64le',
+  'linux-s390x': 'linux-s390x',
+  'linux-x64': 'linux-64',
+  'win32-x64': 'win-64',
+} as const;
---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import { exit } from 'process';
+import * as core from '@actions/core';
+import { getDownloadUrlFromApi } from './anaconda-api';
+import { downloadCondaStandalone } from './download';
+import { getOptions } from './options';
+
+/**
+ * Runs the action and sets outputs and environment variables.
+ */
+const run = async (): Promise<void> => {
+  const options = getOptions();
+  const downloadUrl =
+    options.downloadUrl ??
+    (await getDownloadUrlFromApi(
+      options.buildString,
+      options.channel!,
+      options.condaVersion,
+      options.platform,
+    ));
+  const standaloneBin = await downloadCondaStandalone(
+    downloadUrl,
+    options.destinationDirectory,
+  );
+  core.exportVariable('CONDA_EXE', standaloneBin);
+};
+
+run()
+  .then(() => exit(0)) // workaround for https://github.com/actions/toolkit/issues/1578
+  .catch((err: unknown) => {
+    if (core.isDebug()) {
+      throw err;
+    }
+    if (err instanceof Error) {
+      core.setFailed(err.message);
+      exit(1);
+    } else if (typeof err === 'string') {
+      core.setFailed(err);
+      exit(1);
+    }
+    throw err;
+  });
